d9a6d443d4b7dbf6713a8714a1c69275c7c123f9
Naoya Hieda <64737710+naoyahieda@users.noreply.github.com>
2022-01-29 16:23:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ def handler(event, context):
     conn = get_connection()
     cur = conn.cursor()
 
-    # SQL実行（tbl_sampleから全データを取得）
     cur.execute('SELECT * FROM item')  # ここでなぜかエラーがでる
     rows = cur.fetchall()
     print(rows)
